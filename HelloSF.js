da20e8479156a9413b9a2604ff106cb55aa5893f
nli12 <nathanli117@gmail.com>
2018-06-01 14:05:18 -0700

    new changes

--- HelloSF.js
+++ HelloSF.js
@@ -1,6 +1,7 @@
 console.log("Bienvenidos a San Francisco"); 
 
 
-var x = [1, 2, 4]; 
+var x = [1, 2, 3, 4]; 
 x.forEach((item) => {console.log(item)}); 
 var x = "I thought the future would be cooler"; 
+var y = "Here I go again"
